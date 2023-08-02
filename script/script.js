951f83d9c363d32eac463e2003783aa02234a154
Felilpz <62868463+Felilpz@users.noreply.github.com>
2023-08-02 20:50:16 -0300

    wip

--- script/script.js
+++ script/script.js
@@ -1,12 +1,45 @@
 const btn = document.getElementById('botao')
 
-let fonte = document.getElementById('credor').value
-let montante = Number(document.getElementById('valor').value)
-let detalhes = String(document.getElementById('valor'))
+let fonte = document.getElementById('credor')
+let montante = document.getElementById('valor')
+let detalhes = document.getElementById('detalhes')
+let vencimento = document.getElementById('vencimento')
+let transacao = document.getElementById('transacao')
 
 
 btn.addEventListener('click', function() {
     
-    console.log(fonte, montante, detalhes)
+    // console.log(`fonte: ${fonte.value}`)
+    // console.log(`montante: ${montante.value}`)
+    // console.log(`detalhes: ${detalhes.value}`)
+    // console.log(`vencimento: ${vencimento.value}`)
+    // console.log(`transacao: ${transacao.value}`)
 
+    const pagar = document.createElement('div')
+    pagar.className = "item-2"
+    
+    let fonteJS = document.createElement('p')
+    fonteJS.innerHTML = `${fonte.value.toUpperCase()}`
+    pagar.appendChild(fonteJS)
+
+    let montanteJS = document.createElement('p')
+    montanteJS.innerHTML = `R$ ${montante.value.toUpperCase()}`
+    pagar.appendChild(montanteJS)
+
+    let detalhesJS = document.createElement('p')
+    detalhesJS.innerHTML = `${detalhes.value.toUpperCase()}`
+    pagar.appendChild(detalhesJS)
+
+    let vencimentoJS = document.createElement('p')
+    vencimentoJS.innerHTML = `${transacao.value}`
+    pagar.appendChild(vencimentoJS)
+
+    let transacaoJS = document.createElement('p')
+    transacaoJS.innerHTML = `${vencimento.value}`
+    pagar.appendChild(transacaoJS)
+
+    pagar.innerHTML += `<i class="bi bi-check2-all btn-check"></i>`
+
+    const divPai = document.getElementById('aPagar')
+    divPai.appendChild(pagar)
 })
