934996008d4df8f88b57798bf6fb939ee3b9318f
Felilpz <62868463+Felilpz@users.noreply.github.com>
2023-08-10 14:50:48 -0300

    WIP

--- script/script.js
+++ script/script.js
@@ -9,6 +9,8 @@ let saida = document.getElementById('radioSaida')
 const divPai = document.getElementById('aPagar')
 let banco = []
 
+const title = document.getElementById('titulo')
+
 
 const form = document.querySelector('#form')
 
@@ -34,68 +36,41 @@ form.addEventListener('submit', (event) => {
         window.alert(`Por favor, preencha o campo TRANSACAO`)
         return;
     }
-
-    const entradaSaida = document.getElementsByName('radio')
-    let opcao
-    for(c = 0; c < entradaSaida.length; c++) {
-        if(entradaSaida[c].checked) {
-            opcao = entradaSaida[c].value
-            console.log(opcao)
-            break
-        }
-
-        if(entradaSaida == 'saida') {
-
-            const pagar = document.createElement('div')
-            pagar.className = "item-2"
-
-            let fonteJS = document.createElement('p')
-            fonteJS.textContent = `${fonte.value}`
-            pagar.appendChild(fonteJS)
-
-            let montanteJS = document.createElement('p')
-            montanteJS.innerHTML = `<span class="valores">R$ ${montante.value}</span>`
-            pagar.appendChild(montanteJS)
-
-            let detalhesJS = document.createElement('p')
-            detalhesJS.textContent = `${detalhes.value}`
-            pagar.appendChild(detalhesJS)
-
-            let transacaoJS = document.createElement('p')
-            let converterData = transacao.value.split('-').reverse().join('/')
-            transacaoJS.textContent = `${converterData}`
-            pagar.appendChild(transacaoJS)
-
-            pagar.innerHTML += `<i class="bi bi-check2-all btn-check"></i>`
-
-            divPai.appendChild(pagar)
-            }
-    }
-
-
+    
     // armazenando montante em um array e fazend soma para despesas
     banco.push(parseFloat(montante.value))
     console.log(banco)
     let soma = 0
-    for(c = 0; c < banco.length; c++) {
+    for (c = 0; c < banco.length; c++) {
         soma += Number(banco[c])
-    }
-    console.log(soma)
-
-    form.reset()
-    atualizarDespesas()
-})
+}})
 
 //despesas (preciso pegar a div pagas e percorrer todos os valores(classe montante) e dps somar todas elas)
 function atualizarDespesas() {
     let inputReceita = document.getElementById('entradas').value
-   
+
     banco.push(parseFloat(montante.value))
     let soma = 0
-    for(c = 0; c < banco.length; c++) {
+    for (c = 0; c < banco.length; c++) {
         soma += Number(banco[c])
     }
-    
+
     let despesas = document.getElementById('despesas')
     despesas.innerText = soma
 }
+
+function escolherTela() {
+    // checar qual input radio está clicado e mostrar a tela
+    if (document.getElementById('entradasId').checked) {
+        title.innerText = "ENTRADAS"
+        console.log('Entrada')
+    } else if(document.getElementById('pegarId').checked) {
+        title.innerText = "PAGAR"
+        console.log('Pagar')
+    } else {
+        title.innerText = "PAGAS"
+        console.log('Pagas')
+    }
+}
+
+escolherTela();
